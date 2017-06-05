0ff3dcb2208d4ac6d0997e6f334e3cee205383a6
Sergey Gavruk <svgavruk@gmail.com>
2017-06-05 17:24:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const emptyFn = () => {};
+
+class TelegramLogger {
+  constructor({
+    name, token, password, chatIds,
+    onNewChat,
+  }) {
+    this.chatIds = chatIds || [];
+    this.onNewChat = onNewChat || emptyFn;
+
+    this.bot = new TelegramBot(token, { polling: true });
+
+    this.handleAuth(name, password);
+  }
+
+  handleAuth(name, password) {
+    this.bot.onText(/\/auth (.+)/, (msg, match) => {
+      const chatId = msg.chat.id;
+      const authPassword = match[1];
+
+      if (authPassword === password) {
+        if (!this.chatIds.find(x => x === chatId)) {
+          this.chatIds.push(chatId)
+        }
+        this.bot.sendMessage(chatId, `Subscribed to ${name}`);
+      }
+    });
+  }
+
+  fnLog(...args) {
+    this.chatIds.forEach(chatId => {
+      this.bot.sendMessage(chatId, args.join(' '));
+    });
+  }
+
+  log(...args) {
+    this.fnLog('[LOG] ', new Date(), ...args);
+  }
+
+  error(...args) {
+    this.fnError('[ERROR] ', new Date(), ...args);
+  }
+
+  debug(...args) {
+    this.fnDebug('[DEBUG] ', new Date(), ...args);
+  }
+}
+
+module.exports = TelegramLogger;
